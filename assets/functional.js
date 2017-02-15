6be0c4baedb2d5deb1bb8b75db486903266c3986
Dana Iliescu <dana.iliescu96@gmail.com>
2017-02-15 23:33:59 +0100

    hello message with images as background

--- assets/functional.js
+++ assets/functional.js
@@ -1,1 +1,64 @@
-functional.js
+window.onload = function() {
+  showContent();
+  smoothScroll();
+  loadImages();
+};
+
+//fade in the home page
+function showContent() {
+  $('#page_container').removeClass('fade-out'); //fade-in the body
+  setTimeout(function() {$('#hello').removeClass('fade-out');}, 500); //fade-in the title(slow)
+}
+
+//add smooth scroll effect when jumping to sections
+function smoothScroll() {
+  var $root = $('html, body');
+  $('a.scrolly').bind('click', function(event) {
+      var $anchor = $(this);
+      $root.stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+  });
+}
+
+//STORE PICTURES IN ARRAY
+//SET BACKGROUND-IMAGE FROM HERE, FROM INSIDE A LOOP
+//SET TIME OUT TO THE METHOD TO 2 3 SECONDS
+var imgArray = [];
+function loadImages(){
+	var folder = "assets/img/";
+	$.ajax({
+    	url : folder,
+    	async: false,
+    	success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+            	imgArray.push(val); 
+            } 
+        });
+    	}
+	});
+	console.log(imgArray);
+}
+
+// change the image every 3 seconds            
+var i = 0; 
+function myLoop () {          
+   setInterval(function () {   
+      $('#hello_title').css({
+			'background': 'url(assets/img/' + imgArray[i] + ') no-repeat',
+			'-webkit-background-clip' : 'text',
+			'-webkit-text-fill-color' : 'transparent'
+		});
+		i++;                     
+      	if (i >= imgArray.length) { 
+        	i = 0;             
+      	} 
+   	}, 2000);
+}
+
+myLoop();
+
+
+document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>');
